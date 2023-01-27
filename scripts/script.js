5ca248f095841e1eabe1c28df798c8cf9db8bf72
fer_kuster <fernandoarielkuster@gmail.com>
2023-01-27 18:06:35 -0300

    Add js functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -28,7 +28,9 @@ if (localStorage.cantidadDeIngresos) {
   document.getElementById("contadorDeIngresos").innerHTML = "Se ingresó al sitio "+cantidadDeIngresos+" vez";
 }
 
+
 /* Última visita */
+
 if (localStorage.ultimaVisita == undefined) {
     document.getElementById("ultimaVisita").innerHTML = "Primer visita";
     console.log("Primer visita" + localStorage.ultimaVisita);
@@ -43,3 +45,33 @@ let datetime = currentdate.getDate() + "/"
             + currentdate.getMinutes() + ":" 
             + currentdate.getSeconds();
 localStorage.ultimaVisita = datetime;
+
+
+/* Json */
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let firstName;
+let lastName;
+let email;
+let phoneNumber;
+
+function getData() {
+  let val = getRandomInt(8) + 1;
+  let user = "https://dummyjson.com/users/" + val;
+  console.log(user);
+  return fetch(user)
+    .then(response => response.json())
+    .then(data => {
+      firstName = data.firstName;
+      lastName = data.lastName;
+      email = data.email;
+    });
+}
+
+getData().then(() => {
+  document.getElementById("Nombre").innerHTML = "Nombre: "+ firstName + " " + lastName;
+  document.getElementById("Email").innerHTML = "Email: "+ email;
+});
