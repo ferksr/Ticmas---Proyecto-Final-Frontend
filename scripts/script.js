01ddf34f0ce6d6aa7d4e6881f1b50317c0686277
fer_kuster <fernandoarielkuster@gmail.com>
2023-01-27 17:18:31 -0300

    Adds js

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+
+/* Cambiar color de titulos*/
+
+console.log(document.getElementById("colorDeTitulos").value);
+console.log(document.getElementById("cambiarColor").innerHTML);
+document.getElementById("cambiarColor").addEventListener("click", fnCambiarColor);
+
+function fnCambiarColor() {
+  let valorColorDeTiutlos = document.getElementById("colorDeTitulos").value
+  console.log(valorColorDeTiutlos);
+  const collection = document.getElementsByTagName("h2");
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].style.color = valorColorDeTiutlos;
+  }
+}
+
+
+/* Visitas al sitio */
+
+if (localStorage.cantidadDeIngresos) {
+  let cantidadDeIngresos = localStorage.cantidadDeIngresos;
+  cantidadDeIngresos++;
+  localStorage.cantidadDeIngresos = cantidadDeIngresos;
+  document.getElementById("contadorDeIngresos").innerHTML = "Se ingresó al sitio "+cantidadDeIngresos+" veces";
+} else {
+  let cantidadDeIngresos = 1;
+  localStorage.cantidadDeIngresos = cantidadDeIngresos;
+  document.getElementById("contadorDeIngresos").innerHTML = "Se ingresó al sitio "+cantidadDeIngresos+" vez";
+}
+
+/* Última visita */
+if (localStorage.ultimaVisita == undefined) {
+    document.getElementById("ultimaVisita").innerHTML = "Primer visita";
+    console.log("Primer visita" + localStorage.ultimaVisita);
+  } else {
+    document.getElementById("ultimaVisita").innerHTML = "Ultima visita: " + localStorage.ultimaVisita;
+  }
+let currentdate = new Date(); 
+let datetime = currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear() + " @ "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+localStorage.ultimaVisita = datetime;
